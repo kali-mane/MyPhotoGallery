6bbbf6cd2fe7fb87333990b0d3cf119c8c1fb506
maneeshavinayak <32324416+maneeshavinayak@users.noreply.github.com>
2017-11-09 21:26:39 +0530

    Error fix in form

--- photos/forms.py
+++ photos/forms.py
@@ -13,19 +13,34 @@ class ImagesForm(forms.ModelForm):
         
 
 class CommentsForm(forms.ModelForm):
-    image_name = forms.CharField(required=False,widget=forms.HiddenInput())
+    image_name = forms.ModelChoiceField(queryset=Images.objects.none(), required=False, widget=forms.HiddenInput())
     comment = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'name' : 'comment'}))
- #   comment_dt = forms.DateTimeField()
+#   comment_dt = forms.DateTimeField()
     
     class Meta:
         model = Comments
-        fields = ('image_name', 'comment',)   
-    
-#    def __init__(self, *args, **kwargs):
-#        image_name=kwargs.pop('image_name','')
-#        super(CommentsForm, self).__init__(*args, **kwargs)
-#        self.fields['image_name']=forms.ModelChoiceField(queryset=Images.objects.filter(image_name=image_name))
-         
+        fields = ('image_name', 'comment',)       
+        
+    def __init__(self, image_name, *args, **kwargs):
+        image = kwargs.get('image_name')
+        super(CommentsForm, self).__init__(*args, **kwargs)
+        self.fields['image_name'] = Images.objects.filter(image_name=image)     
+ 
+  
+'''            
+    def save(self, commit=True):
+        instance = super(CommentsForm, self).save(commit=False)
+#        instance.comment = self.cleaned_data['comment']
+#        instance.fields['image_name'].queryset = Images.objects.filter(image_name=request.image_name)
+#        instance.image_name
+#        image_name_input = self.cleaned_data['image_name']
+#        image_name_fm = image_name_input.replace("'", '')
+#        image = Images.objects.get(image_name=image_name_fm)
+#        instance.image_name = image.image_name
+        if commit:
+            instance.save()
+        return instance
+ '''            
     
         
 class LoginForm(AuthenticationForm):
